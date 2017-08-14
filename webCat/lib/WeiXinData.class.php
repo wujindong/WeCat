01a82aafa9a8804b23d9ce74580449a1b3bb2133
wujindong <498298758@qq.com>
2017-08-14 17:28:53 +0800

    '修复bug'

--- webCat/lib/WeiXinData.class.php
+++ webCat/lib/WeiXinData.class.php
@@ -53,7 +53,7 @@ class WeiXinData {
             // 如果是企业号用以下 URL 获取 ticket
             // $url = "https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=$accessToken";
             $url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&access_token=$accessToken";
-            $res = json_decode($this->httpGet($url));
+            $res = json_decode($this->https_request($url));
             $ticket = $res->ticket;
             if ($ticket) {
                 $data->expire_time = time() + 7000;
