4a1a583d387d3baf66bf9b914a6f8977ac6ad18a
wujindong <498298758@qq.com>
2017-08-15 10:00:09 +0800

    '新增一次性订阅消息'

--- webCat/lib/TempleteMessageManager.class.php
+++ webCat/lib/TempleteMessageManager.class.php
@@ -19,4 +19,18 @@ class TempleteMessageManager extends WeiXinData {
         return json_decode($res, TRUE);
     }
 
+
+    /**
+     * 一次性订阅消息
+     * @param $scene 场景值
+     * @param $template_id 模板id
+     * @param $redirect_url 回调地址
+     * @param $reserved 自定义参数原样返回
+     */
+    public function  onlyTemplateMessage($scene,$template_id,$redirect_url,$reserved){
+        $url="https://mp.weixin.qq.com/mp/subscribemsg?action=get_confirm&appid=".$this->appId."&scene=".$scene."&template_id=".$template_id.
+            "&redirect_url=".$redirect_url."&reserved=".$reserved."#wechat_redirect";
+        header("location:".$url);
+    }
+
 }
